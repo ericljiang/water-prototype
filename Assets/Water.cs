7c4543161f1928b04802ae68ac1e760c76728cea
Eric Jiang <eric.jiang2@gmail.com>
2021-09-13 01:16:40 -0700

    Working dynamic mesh.

---
+++ Assets/Water.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [SerializeField] private int width, length;
+    
+    private int vertWidth, vertLength;
+
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vertWidth = width + 1;
+        vertLength = length + 1;
+        
+        var meshFilter = gameObject.AddComponent<MeshFilter>();
+        mesh = new Mesh
+        {
+            vertices =
+            (
+                from x in Enumerable.Range(0, vertWidth)
+                from z in Enumerable.Range(0, vertLength)
+                select new Vector3(x, 0, z)
+            ).ToArray(),
+            triangles =
+            (
+                from x in Enumerable.Range(0, vertWidth - 1)
+                from z in Enumerable.Range(0, vertLength - 1)
+                from index in new List<int>
+                {
+                    x + z * vertWidth,
+                    (x + 1) + (z + 1) * vertWidth,
+                    x + (z + 1) * vertWidth,
+                    x + z * vertWidth,
+                    (x + 1) + z * vertWidth,
+                    (x + 1) + (z + 1) * vertWidth
+                }
+                select index
+            ).ToArray()
+        };
+        mesh.RecalculateNormals();
+        meshFilter.mesh = mesh;
+    }
+
+    void FixedUpdate()
+    {
+        mesh.vertices = (
+            from x in Enumerable.Range(0, vertWidth)
+            from z in Enumerable.Range(0, vertLength)
+            select new Vector3(x, Mathf.Sin(Time.fixedTime + x - z / 2f) / 2, z)
+        ).ToArray();
+        mesh.RecalculateNormals();
+    }
+}
